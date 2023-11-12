5c9741aba0880a55f0450c5eaab6d2f9b7cd5dd1
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-12 22:44:55 +0800

    Add game page

--- header.php
+++ header.php
@@ -1,10 +1,16 @@
 <div class="TopBar">
-<nav>
-    <a><img src="https://placehold.co/25" alt="Website Logo"></a>
+  <nav>
+    <a><img src="https://placehold.co/50" alt="Website Logo"></a>
     <ul>
-        <li><div class="HoverTime"><a href="index.php">Home</div></a></li>
-        <li><div class="HoverTime"><a href="contact.php">Contact Us</div></a></li>
-        <li><div class="HoverTime"><a href="about.php">About Us</div></a></li>
+      <li>
+        <div class="HoverTime"><a href="index.php">Home</div></a>
+      </li>
+      <li>
+        <div class="HoverTime"><a href="contact.php">Contact Us</div></a>
+      </li>
+      <li>
+        <div class="HoverTime"><a href="about.php">About Us</div></a>
+      </li>
     </ul>
-</nav>
+  </nav>
 </div>
