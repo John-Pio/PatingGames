1a45c7d347d65e5582eaf674f135d68b1463f3e1
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-13 14:10:29 +0800

    Organize files Rename folders Add responsive styles

---
+++ components/footer.php
@@ -0,0 +1,31 @@
+<footer class="TopBar">
+    <div class="container">
+        <section class="copyright">
+            <a><img src="https://placehold.co/100" alt="Website Logo"></a>
+            <p>© 2023-<?php echo date("Y"); ?> TeamPating</p>
+            <p>All rights reserved.</p>
+        </section>
+        <section class="categories">
+            <ul>
+                <h3>Link header</h5>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+            </ul>
+        </section>
+        <section class="links">
+            <ul>
+                <h3>Link header</h5>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+                    <li><a href="#">Lorem, ipsum dolor.</a></li>
+            </ul>
+        </section>
+    </div>
+</footer>
---
+++ components/header.php
@@ -0,0 +1,16 @@
+<div class="TopBar">
+    <nav>
+        <a><img src="https://placehold.co/50" alt="Website Logo"></a>
+        <ul class="nav-links">
+            <li>
+                <div class="HoverTime"><a href="index.php">Home</div></a>
+            </li>
+            <li>
+                <div class="HoverTime"><a href="contact.php">Contact Us</div></a>
+            </li>
+            <li>
+                <div class="HoverTime"><a href="about.php">About Us</div></a>
+            </li>
+        </ul>
+    </nav>
+</div>
