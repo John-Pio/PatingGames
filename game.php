cf8b97ff705eb221d56c9af707b4e220f25ea7fc
John-Pio <geofreygapasin@gmail.com>
2023-11-19 16:38:36 +0800

    Directory Problems

--- game.php
+++ game.php
@@ -2,20 +2,19 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/gamepage.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
-    <?php require 'components/header.php' ?>
-    <main>
-        <iframe src="games/2048.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="1280px" height="720px">
-        </iframe>
-    </main>
-    <?php require 'components/footer.php' ?>
+  <?php require 'components/header.php' ?>
+  <main>
+    <iframe src="games/2048.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="1280px" height="720px">
+    </iframe>
+  </main>
+  <?php require 'components/footer.php' ?>
 </body>
 
 </html>
