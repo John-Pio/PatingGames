a1972882e8b48d4a2660dccb1f3e669ac21a77a4
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-19 16:22:01 +0800

    Move files Create asset folder

---
+++ game.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/gamepage.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <?php require 'components/header.php' ?>
+    <main>
+        <iframe src="games/2048.html" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" width="1280px" height="720px">
+        </iframe>
+    </main>
+    <?php require 'components/footer.php' ?>
+</body>
+
+</html>
