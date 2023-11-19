50e2b35b38a4c0f245cac6f2d11016fefed6e933
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-19 16:26:31 +0800

    Change title Link style

--- index.php
+++ index.php
@@ -2,151 +2,105 @@
 
 <head>
     <meta name="Viewport" content="width=device-width, initial-scale=1">
-    <title>TeamPating Website</title>
+    <title>Home | PatingGames</title>
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/homepage.css">
 </head>
 
 <body>
     <?php require 'components/header.php' ?>
     <main>
-        <ul>
+        <ul role="list">
             <li class="card">
                 <a href="game.php">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
             <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
+                <a href="game.php">
+                    <div class="thumbnail">
+                        <img src="https://placehold.co/200" alt="Game Thumbnail">
+                    </div>
                     <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
+                        <h4>Lorem, ipsum.</h4>
+                        <p>Lorem ipsum dolor sit.</p>
                     </div>
                 </a>
             </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-            <li class="card">
-                <a href="#">
-                    <img src="https://placehold.co/200" alt="Game Thumbnail">
-                    <div class="info">
-                        <h3>Game Title</h3>
-                        <p>Created using HTML5</p>
-                    </div>
-                </a>
-            </li>
-        </ul>
+
     </main>
     <?php require 'components/footer.php' ?>
 </body>
