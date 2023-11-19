d002f8af22fe8c38f2fa81b0867439058ec01ebe
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-19 16:25:31 +0800

    Link site components and styles

--- about.php
+++ about.php
@@ -1,5 +1,21 @@
-<?php
-echo 'geofrey';
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-<h1>Pating Website</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About | PatingGames</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/about.css">
+</head>
+
+<body>
+    <?php require "components/header.php" ?>
+    <main>
+
+    </main>
+    <?php require "components/footer.php" ?>
+</body>
+
+</html>
--- contact.php
+++ contact.php
@@ -1,6 +1,21 @@
-<?php
-echo 'geofrey';
-echo 'bruh';
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-<h1>Pating Website</h1>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us | PatingGames</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/contact.css">
+</head>
+
+<body>
+    <?php require "components/header.php" ?>
+    <main>
+
+    </main>
+    <?php require "components/footer.php" ?>
+</body>
+
+</html>
