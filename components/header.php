b3f3a6bba9fb2bc8bcc8e1ddfcf7948ef93efb48
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-20 11:20:17 +0800

    Add menu in mobile view

--- components/header.php
+++ components/header.php
@@ -1,16 +1,26 @@
-<nav>
-    <div class="container">
-        <a><img src="https://placehold.co/50" alt="Website Logo"></a>
-        <ul role="list">
-            <li>
-                <a href="index.php">Home</a>
-            </li>
-            <li>
-                <a href="contact.php">Contact Us</a>
-            </li>
-            <li>
-                <a href="about.php">About Us</a>
-            </li>
-        </ul>
+<header>
+  <nav>
+    <div>
+      <a><img src="https://placehold.co/50" alt="Website Logo"></a>
+      <img id="menubr" src="./assets/img/menu.svg" alt="Menu Icon">
     </div>
-</nav>
+    <ul id="navlnk" role="list">
+      <li>
+        <a href="index.php">Home</a>
+      </li>
+      <li>
+        <a href="contact.php">Contact Us</a>
+      </li>
+      <li>
+        <a href="about.php">About Us</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<script>
+  const menu = document.getElementById('menubr')
+  const links = document.getElementById('navlnk')
+
+  menu.addEventListener('click', () => links.classList.toggle('active'))
+</script>
