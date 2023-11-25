735a9d63ca0fbd78e2b528e8a8f6a40f75437ae9
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-25 23:41:26 +0800

    Add icon links

--- components/footer.php
+++ components/footer.php
@@ -1,31 +1,40 @@
 <footer>
-    <div class="container">
-        <section class="copyright">
-            <a><img src="https://placehold.co/100" alt="Website Logo"></a>
-            <p>© 2023-<?php echo date("Y"); ?> TeamPating</p>
-            <p>All rights reserved.</p>
-        </section>
-        <section class="categories">
-            <h4>Link header</h4>
-            <ul role="list">
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-            </ul>
-        </section>
-        <section class="links">
-            <h4>Link header</h4>
-            <ul role="list">
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-                <li><a href="#">Lorem, ipsum dolor.</a></li>
-            </ul>
-        </section>
-    </div>
+  <div class="container">
+    <section class="copyright">
+      <a><img src="https://placehold.co/100" alt="Website Logo"></a>
+      <p>© 2023-<?php echo date("Y"); ?> TeamPating</p>
+      <p>All rights reserved.</p>
+    </section>
+    <section class="categories">
+      <h4>Link header</h4>
+      <ul role="list">
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+      </ul>
+    </section>
+    <section class="links">
+      <h4>Link header</h4>
+      <ul role="list">
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+        <li><a href="#">Lorem, ipsum dolor.</a></li>
+      </ul>
+    </section>
+    <section class="icons">
+      <h4>Follow Us</h4>
+      <ul role="list">
+        <li><a href=""><img src="./assets/img/facebook-logo.svg" alt=""></a></li>
+        <li><a href=""><img src="./assets/img/x-logo.svg" alt=""></a></li>
+        <li><a href=""><img src="./assets/img/discord-logo.svg" alt=""></a></li>
+        <li><a href=""><img src="./assets/img/github-logo.svg" alt=""></a></li>
+      </ul>
+    </section>
+  </div>
 </footer>
