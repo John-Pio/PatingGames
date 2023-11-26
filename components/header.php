9f8a79f71f8b721644f27549a8ded456137db067
thisellie <81550623+thisellie@users.noreply.github.com>
2023-11-26 14:36:08 +0800

    Add dark theme

--- components/header.php
+++ components/header.php
@@ -1,20 +1,23 @@
 <header>
   <nav>
-    <div>
+    <div class="left">
       <a><img src="https://placehold.co/50" alt="Website Logo"></a>
       <img id="menubr" src="./assets/img/menu.svg" alt="Menu Icon">
     </div>
-    <ul id="navlnk" role="list">
-      <li>
-        <a href="index.php">Home</a>
-      </li>
-      <li>
-        <a href="contact.php">Contact Us</a>
-      </li>
-      <li>
-        <a href="about.php">About Us</a>
-      </li>
-    </ul>
+    <div class="right">
+      <ul id="navlnk" role="list">
+        <li>
+          <a href="index.php">Home</a>
+        </li>
+        <li>
+          <a href="contact.php">Contact Us</a>
+        </li>
+        <li>
+          <a href="about.php">About Us</a>
+        </li>
+        <li><img id="toggle" src="./assets/img/dark-mode.svg" alt="Theme Icon"></li>
+      </ul>
+    </div>
   </nav>
 </header>
 
