2f3eada663cd3ca907b741ce623800e3f887241b
thisellie <81550623+thisellie@users.noreply.github.com>
2023-12-02 20:27:29 +0800

    Rename index to home Add filter function

--- components/header.php
+++ components/header.php
@@ -4,7 +4,7 @@
       <a href="index.php"><img width="50px" height="50px" src="./assets/img/SharkLogo.svg" alt="Website Logo"></a>
       <img id="menubr" src="./assets/img/menu.svg" alt="Menu Icon">
       <?php
-      if (basename($_SERVER['PHP_SELF']) === 'index.php')
+      if (basename($_SERVER['PHP_SELF']) === 'home.php')
         echo "<form method='get'>
           <input type='text' name='search' id='search' placeholder='Search a game!'>"; ?>
       </form>
@@ -12,7 +12,7 @@
     <div class="right">
       <ul id="navlnk" role="list">
         <li>
-          <a href="index.php">Home</a>
+          <a href="home.php">Home</a>
         </li>
         <li>
           <a href="contact.php">Contact Us</a>
---
+++ home.php
@@ -0,0 +1,144 @@
+<?php
+// Sets the dark mode cookie if does not exist
+$name = "dark";
+if (!isset($_COOKIE[$name]))
+  setcookie($name, "false", strtotime('Thu, 18 Dec 2030 12:00:00'), "/PatingGames");
+
+$games = array(
+  (object) [
+    'title' => '2048',
+    'desc' => 'A 2048 website game created by Fabian Richter',
+    'tags' => ['casual', 'puzzle']
+  ], (object) [
+    'title' => 'Canvas Tetris',
+    'desc' => 'A tetris website game created by Dionysis Zindros',
+    'tags' => ['casual', 'arcade']
+  ], (object) [
+    'title' => 'Monster Eating',
+    'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    'tags' => ['casual']
+  ], (object) [
+    'title' => 'There Is No Game',
+    'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    'tags' => ['puzzle']
+  ], (object) [
+    'title' => 'Ultra Pixel Survive',
+    'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    'tags' => ['shooter']
+  ], (object) [
+    'title' => 'Pixel Bear Adventure',
+    'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    'tags' => ['adventure']
+  ], (object) [
+    'title' => 'Red Tie Runner',
+    'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+    'tags' => ['racing', 'arcade']
+  ]
+);
+?>
+
+
+<!DOCTYPE html>
+
+<head>
+  <meta name="Viewport" content="width=device-width, initial-scale=1">
+  <title>Home | PatingGames</title>
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="assets/css/homepage.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <script src="script.js" defer></script>
+
+
+</head>
+
+<body>
+  <?php require 'components/header.php' ?>
+  <main>
+    <ul id="categories" role="list">
+      <?php
+      // For echoing the category buttons
+      $buttons = array("arcade", "adventure", "casual", "sports", "puzzle", "racing", "shooter");
+
+      foreach ($buttons as $name) {
+        echo "<li><form class='{$name}' method='post'><img src='assets/img/categories/{$name}.svg' alt='Category Icon'><input class={$name} type='submit' name='category' value=" . ucfirst($name) . "></input></form></li>";
+      }
+      ?>
+      <li>
+        <form class="clear" method="post"><input type="submit" name="clear" value="Clear"></form>
+    </ul>
+    <ul id="list" role="list">
+      <?php
+      // For clicking the clear button
+      if (isset($_POST['clear'])) {
+        clearGames();
+        loadGames();
+      }
+
+      // For clicking from the categories
+      if (isset($_POST['category'])) {
+        searchGames('category', lcfirst($_POST['category']));
+      }
+
+      // Searching at the search bar
+      if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        if (isset($_GET['search'])) {
+          $query = htmlentities($_GET['search']);
+          searchGames('search', $query);
+        } else loadGames();
+      }
+
+      function renderGames($game)
+      {
+        echo "<li class='card'>
+        <div class='thumbnail'>
+            <img src='https://placehold.co/200' alt='Game Thumbnail'>
+        </div>
+        <div class='info'>
+            <h4>{$game->title}</h4>
+            <p>{$game->desc}</p>
+            <form action='game.php' method='get'>
+                <button name='PlayButton' value='{$game->title}' type='submit'>PLAY</button>
+            </form>
+        </div>
+    </li>";
+      }
+
+      function searchGames($location, $query)
+      {
+        clearGames();
+
+        $filtered = [];
+
+        if ($location == 'category') {
+          $filtered = array_filter($GLOBALS['games'], function ($game) use ($query) {
+            return in_array($query, $game->tags);
+          });
+        } else {
+          $filtered = array_filter($GLOBALS['games'], function ($game) use ($query) {
+            return str_contains($game->title, $query);
+          });
+        }
+        foreach ($filtered as $game) renderGames($game);
+      }
+
+      function loadGames()
+      {
+        foreach ($GLOBALS['games'] as $game) {
+          renderGames($game);
+        }
+      }
+
+      function clearGames()
+      {
+        echo "<script>
+        const parent = document.getElementById('list');
+        parent.innerHTML = '';
+        </script>";
+      }
+      ?>
+      <a class="backtop" href="#"><img src="./assets/img/backtop.svg" alt="Back to Top"></a>
+  </main>
+  <?php require 'components/footer.php' ?>
+</body>
+
+</html>
