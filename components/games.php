d0175892d72429afeee08280936f37b8ed694454
Markpats <markpatani21@gmal.com>
2023-12-04 11:28:02 +0800

    component-game patch.

--- components/games.php
+++ components/games.php
@@ -6,68 +6,258 @@ $games = array(
     'descp' => 'Strategic incremental game.',
     'tags' => ['puzzle'],
     'link' => 'https://codepen.io/fabi_yo_/embed/zNrmwZ?default-tab=html%2Cresul',
+    'descDetails' => '<div>
+                        <h4 style="text-align: center;">Game Description</h4>
+                        <a style="text-align: center; 10px;">Engage your mind in the captivating 2048 game, a strategic puzzle that demands both skill and foresight. Swipe and combine numbered tiles on a 4x4 grid, aiming to reach the coveted 2048 tile. With each move, a new tile appears, and every decision matters. Can you outwit the grid and achieve the ultimate numerical feat? Its a minimalist design with maximum challenge!</a>
+                      </div>',
+
+    'tagDetails' => ' <div class="tags style="text-alight: right;">
+                        <h4 style="text-align: center;">Tags</h4>
+                        <a style="margin-left: 10px;">1 Player,</a>
+                        <a style="margin-left: 10px;">Matching,</a>
+                        <a style="margin-left: 10px;">Math,</a>
+                        <a style="margin-left: 10px;">Mouse Skill,</a>
+                        <a style="margin-left: 10px;">Puzzle</a>
+                      </div> ',
+
+    
   ],
   (object)[
     'title' => 'Canvas Tetris',
     'descp' => 'Classic Game remade for the web.',
     'tags' => ['puzzle'],
     'link' => 'https://dionyziz.com/graphics/canvas-tetris/',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Challenges you to strategically arrange falling Tetriminos to complete horizontal lines. Easy to pick up but endlessly engaging, Tetris offers a thrilling test of your skills as the game speeds up. Whether youre a seasoned player or new to the Tetris world, experience the addictive joy of perfecting your block-stacking strategy in this classic game that continues to captivate players globally.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Block,</a>
+    <a style="margin-left: 10px;">Tetris,</a>
+    <a style="margin-left: 10px;">Thinking,</a>
+    <a style="margin-left: 10px;">Puzzle</a>
+  </div> ',
+          
   ],
   (object)[
     'title' => 'Monster Eating',
     'descp' => 'Eat smaller monsters and avoid bigger ones!.',
     'tags' => ['arcade', 'featured'],
     'link' => 'https://games.construct.net/941/latest',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">"Monster Eating" unleashes a voracious gaming experience where you control a hungry monster on a devouring spree! Navigate through diverse landscapes, strategically consuming enemies to grow in size and strength. Avoid formidable adversaries and conquer challenges as you satisfy your monsters insatiable appetite. With each successful feast, unlock new levels and power-ups. Dive into the thrill of relentless monster eating and see how mighty you can become in this action-packed game!</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Monster,</a>
+    <a style="margin-left: 10px;">Eating,</a>
+    <a style="margin-left: 10px;">Survival,</a>
+    
+  </div> ',
+        
   ], (object) [
     'title' => 'There Is No Game',
     'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
     'tags' => ['puzzle'],
-    'link' => 'https://games.construct.net/174/latest'
+    'link' => 'https://games.construct.net/174/latest',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">There Is No Game</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">Not a Game,</a>
+    <a style="margin-left: 10px;">Really not a game,</a>
+    <a style="margin-left: 10px;">definitely not a game,</a>
+  </div> ',
+    
   ], (object) [
     'title' => 'Ultra Pixel Survive',
     'descp' => 'Build barricades, furnaces and anvils to upgrade your weapons, collect resources like wood, stone and ores and evolve your favorite heroes!.',
     'tags' => ['shooter'],
-    'link' => 'https://games.construct.net/26420/latest'
+    'link' => 'https://games.construct.net/26420/latest',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Try to survive as long as you can in this amazing game that mixes action RPG and survival
+
+    Build barricades, furnaces and anvils to upgrade your weapons, collect resources like wood, stone and ores and evolve your favorite heroes!
+    
+    Tired of bad games that fill your screen with advertisements? your problems are over!
+    
+    In Ultra Pixel Survive you have fun and your experience is not hindered by inconvenient ads!
+    
+    You only watch ads when you want gold and gems.
+    
+    Ultra Pixel Survival was developed with great care to innovate in the midst of so many bad and opportunistic mobile games.s</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Survival,</a>
+    <a style="margin-left: 10px;">Pixel,</a>
+    <a style="margin-left: 10px;">Open-World</a>
+    <a style="margin-left: 10px;">Adventure</a>
+    <a style="margin-left: 10px;">2d</a>
+  </div> ',
+    
   ], (object) [
     'title' => 'Pixel Bear Adventure',
     'descp' => 'Join Pixel Bear on an epic adventure! Help him find his girlfriend, Peppermint!',
     'tags' => ['adventure'],
-    'link' => 'https://games.construct.net/904/latest'
+    'link' => 'https://games.construct.net/904/latest',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Join Pixel Bear on an epic adventure! Help him find his girlfriend, Peppermint the Polar Bear!</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Adventure,</a>
+    <a style="margin-left: 10px;">Keyboard Skill,</a>
+    <a style="margin-left: 10px;">2d</a>
+    
+  </div> ',
+    
   ], (object) [
     'title' => 'Red Tie Runner',
     'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
     'tags' => ['racing', 'arcade'],
-    'link' => 'https://games.construct.net/1463/latest'
+    'link' => 'https://games.construct.net/1463/latest',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Red Tie Runner is a simplistic, yet challenging, reflex-based, 2D platforming game. Play as a stickman with a red tie to dash and jump your way through obstacles. Features 30 intense levels, including 6 challenge levels, accompanied by spikes, trampolines, ziplines, wingsuits, lava, and more.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Matching,</a>
+    <a style="margin-left: 10px;">Running,</a>
+    <a style="margin-left: 10px;">Keyboard Skill,</a>
+    <a style="margin-left: 10px;">Parkour</a>
+  </div> ',
+
   ], (object) [
     'title' => 'Fireboy and Watergirl 1: Forest Temple',
     'descp' => 'Fireboy and Watergirl 1 is the first cooperative platformer game in the Fireboy and Watergirl series',
     'tags' => ['adventure', 'puzzle'],
-    'link' => 'https://www.gameflare.com/embed/fireboy-and-watergirl-forest-temple'
+    'link' => 'https://www.gameflare.com/embed/fireboy-and-watergirl-forest-temple',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Explore the Forest Temple in search of diamonds. Switch between Fireboy & Watergirl and play alone or together as 2 players. The goal is to reach the exit safely, so be careful. Fireboy cannot touch water and Watergirl cannot touch fire.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">2 Player,</a>
+    <a style="margin-left: 10px;">Matching,</a>
+    <a style="margin-left: 10px;">Math,</a>
+    <a style="margin-left: 10px;">Keyboard Skill,</a>
+    <a style="margin-left: 10px;">Puzzle</a>
+  </div> ',
+
   ], (object) [
     'title' => 'Age of War 2',
     'descp' => 'The game takes you back to different warfare ages. You start with the Stone Age where there are dinosaurs, stone, meteor showers, and cavemen.',
     'tags' => ['action', 'strategy'],
-    'link' => 'https://www.gameflare.com/embed/age-of-war-2/'
+    'link' => 'https://www.gameflare.com/embed/age-of-war-2/',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">WAR ACROSS THE AGES! Train a massive army from cavemen riding dinosaurs to World War tanks! All the way to hugely devastating robot warriors from the future age! There are so many different units to train across 7 completely unique ages of war.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Matching,</a>
+    <a style="margin-left: 10px;">Army,</a>
+    <a style="margin-left: 10px;">Mouse Skill,</a>
+    <a style="margin-left: 10px;">Tower Defense</a>
+  </div> ',
+
   ], (object) [
     'title' => 'Knife Hit',
     'descp' => 'A great little game, with a simple premise and controls. The difficulty scales up as you progress, with fun and straightforward unlocks.',
     'tags' => ['arcade'],
-    'link' => 'https://y8.com/embed/knife_hit_'
+    'link' => 'https://y8.com/embed/knife_hit_',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Throw knives in the wood without loosing any ! You have to beat the bosses from time to time so be very careful as their speed can be very fast !</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Fruit,</a>
+    <a style="margin-left: 10px;">Tap,</a>
+    <a style="margin-left: 10px;">Throwing,</a>
+    <a style="margin-left: 10px;">Mouse Skill</a>
+  </div> ',
+
   ], (object) [
     'title' => 'Gold Miner',
     'descp' => 'Collect gold, stones, and minerals to reach your daily goal.',
     'tags' => ['casual'],
-    'link' => 'https://www.gameflare.com/embed/gold-miner/'
+    'link' => 'https://www.gameflare.com/embed/gold-miner/',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">In this game, you are a miner and you have to earn as much money as you need. Be careful: stones and other objects can slow you down.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Mining,</a>
+    <a style="margin-left: 10px;">Cave,</a>
+    <a style="margin-left: 10px;">Keyboard Skill,</a>
+    <a style="margin-left: 10px;">Catch and Hook</a>
+  </div> ',
+
   ], (object) [
     'title' => 'Farm Frenzy 2',
     'descp' => 'Perfect your farming skills and increase the income of your agricultural complex!',
     'tags' => ['simulation'],
-    'link' => 'https://www.gameflare.com/embed/farm-frenzy-2/'
+    'link' => 'https://www.gameflare.com/embed/farm-frenzy-2/',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Experience the farm life. You will have to take care of all sorts of animals. It is essential that the animals eat. Later, they produce a product which you can then sell in the city. Beware of the bears, which sometimes can attack you. With the money you earn, you can buy new animals for your farm, so try to earn as much as possible. Some are really very expensive.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">1 Player,</a>
+    <a style="margin-left: 10px;">Farming,</a>
+    <a style="margin-left: 10px;">Vegetable,</a>
+    <a style="margin-left: 10px;">Building,</a>
+  </div> ',
+
   ], (object) [
     'title' => '8 Ball Pool',
     'descp' => '8 Ball Pool is an addictive challenging game based on real 3D pool games.',
     'tags' => ['sports'],
-    'link' => 'https://www.gameflare.com/embed/8-ball-pool-multiplayer/'
+    'link' => 'https://www.gameflare.com/embed/8-ball-pool-multiplayer/',
+    'descDetails' => '<div>
+    <h4 style="text-align: center;">Game Description</h4>
+    <a style="text-align: center; 10px;">Online game 8 Ball Pool Multiplayer is one of the most often played pool games on the internet and in mobile phones. At any moment, thousands of players are connected so finding someone to play with wont be a problem. If one of your friends is online, you can play with him or her. At the beginning you will get some coins for free and you can then use them to enter the game. If you win, youll get the opponents coins as well and you will be able to open new parts of the game. The game also brings a tournament in which you have to win three games in a row. With the coins you win, you can unlock new items or various bonuses that will make the game easier to play. If you open the game every day, you can participate in a raffle and win something.</a>
+  </div>',
+
+'tagDetails' => ' <div class="tags style="text-alight: right;">
+    <h4 style="text-align: center;">Tags</h4>
+    <a style="margin-left: 10px;">2 Player,</a>
+    <a style="margin-left: 10px;">Multiplayer game,</a>
+    <a style="margin-left: 10px;">Sports Games,</a>
+    <a style="margin-left: 10px;">Pool Games,</a>
+    <a style="margin-left: 10px;">Social Games</a>
+  </div> ',
   ],
    
 );
