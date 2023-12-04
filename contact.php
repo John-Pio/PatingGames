95221f39c45e28b708fa3f29a3944f2d79137d3c
thisellie <81550623+thisellie@users.noreply.github.com>
2023-12-04 19:13:11 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
+<?php include_once 'components/submit.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,6 +17,53 @@
   <?php require "components/header.php" ?>
   <main>
 
+    <?php if (!empty($statusMsg)) { ?>
+      <div class="status-msg <?php echo $status; ?>"><?php echo $statusMsg; ?>
+        <span class="closebtn">&times;</span>
+      </div>
+
+      <script>
+        var close = document.getElementsByClassName("closebtn");
+        var i;
+
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.opacity = "0";
+            setTimeout(function() {
+              div.style.display = "none";
+            }, 600);
+          }
+        }
+      </script>
+    <?php } ?>
+
+    <form action="" method="post">
+      <div class="contact form">
+        <div class="card">
+          <div class="container">
+            <h2 style="text-align: center;">Contact Us</h2>
+            <div class="form-input">
+              <label for="name">Name:</label>
+              <input type="text" name="name" placeholder="Enter your name" value="<?php echo !empty($postData['name']) ? $postData['name'] : ''; ?>" required="">
+            </div>
+            <div class="form-input">
+              <label for="email">Email:</label>
+              <input type="email" name="email" placeholder="Enter your email" value="<?php echo !empty($postData['email']) ? $postData['email'] : ''; ?>" required="">
+            </div>
+            <div class="form-input">
+              <label for="subject">Subject:</label>
+              <input type="text" name="subject" placeholder="Enter subject" value="<?php echo !empty($postData['subject']) ? $postData['subject'] : ''; ?>" required="">
+            </div>
+            <div class="form-input">
+              <label for="message">Message:</label>
+              <textarea name="message" placeholder="Type your message here" required=""><?php echo !empty($postData['message']) ? $postData['message'] : ''; ?></textarea>
+            </div>
+            <input type="submit" name="submit" class="btn" value="Submit">
+          </div>
+        </div>
+      </div>
+    </form>
   </main>
   <?php require "components/footer.php" ?>
 </body>
